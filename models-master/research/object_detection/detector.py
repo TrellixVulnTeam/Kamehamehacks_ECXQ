f593cccf5f1950c92ef14f740b080383400187f1
njvanhaute <njvanhaute@crimson.ua.edu>
2018-04-15 10:45:23 -0500

    Fixed missing parentheses

--- models-master/research/object_detection/detector.py
+++ models-master/research/object_detection/detector.py
@@ -104,7 +104,7 @@ with detection_graph.as_default():
     stolen = False
     while True:
       #screen = cv2.resize(grab_screen(region=(0,40,1280,745)), (WIDTH,HEIGHT))
-      raw_image = grab_screen(region=X1, Y1, X2, Y2)
+      raw_image = grab_screen(region=(X1, Y1, X2, Y2))
       vertices = np.array([500, 1100], [500, 400], [900, 400], [900, 1100], np.int32)
       masked_image = roi(raw_image, [vertices])
       screen = cv2.resize(masked_image, (800,450))
